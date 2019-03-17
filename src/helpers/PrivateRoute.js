4b0acea1f9edf45b1ea297926c024b4ba15b0026
Asafe Ramos <asafe@asaferamos.com>
2019-03-17 00:48:39 -0300

    adjust of menu

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -15,29 +15,27 @@ export const PrivateRoute = ({ component: Component, ...rest }) => (
                 localStorage.getItem('token') ? (
                     <div>
                         <Menu fixed='top' inverted>
-                            <Container>
-                                <Menu.Item header>
-                                    <Icon name='legal' size='big' />
-                                    ALEAC Meus Projetos Favoritos
-                                </Menu.Item>
-                                <Menu.Item position='right'>
-                                    <Link to="/projetos">
-                                        <Icon name='favorite' size='small' />
-                                        Favoritos
-                                    </Link>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <Link to="/buscar">
-                                        <Icon name='search' size='small' />
-                                        Buscar
-                                    </Link>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <Link to="/logout">
-                                        <Icon name='sign-out' />
-                                    </Link>
-                                </Menu.Item>
-                            </Container>
+                            <Menu.Item header>
+                                <Icon name='legal' size='big' />
+                                ALEAC Meus Projetos Favoritos
+                            </Menu.Item>
+                            <Menu.Item position='right'>
+                                <Link to="/projetos">
+                                    <Icon name='favorite' size='small' />
+                                    Favoritos
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to="/buscar">
+                                    <Icon name='search' size='small' />
+                                    Buscar
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to="/logout">
+                                    <Icon name='sign-out' />
+                                </Link>
+                            </Menu.Item>
                         </Menu>
                         <Component {...props} />
                     </div>
